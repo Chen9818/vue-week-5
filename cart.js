8b0005b96a7e21a6b6d1d930091abbdb856aed24
Chen9818 <88109684+Chen9818@users.noreply.github.com>
2022-02-14 00:24:47 +0800

    fix layout

---
+++ cart.js
@@ -0,0 +1,10 @@
+import { createApp } from 'https://cdnjs.cloudflare.com/ajax/libs/vue/3.0.9/vue.esm-browser.js';
+const app = createApp({
+    data(){
+        return{
+            dd:'aaa'
+        }
+    }
+})
+
+app.mount('#app')
